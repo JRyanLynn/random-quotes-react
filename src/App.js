89f4d1cbfedcb8f6f0414950f580ab0e46ee2598
Jordan Lynn <Jryanlynn@gmail.com>
2022-06-03 21:27:03 -0600

    Committing finished quote gen

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const myArray = [
+  { quote: "Kim K is legit, all caps!", author: "Ray Bradbury" },
+  { quote: "YOLO", author: "Steven Hawking" },
+  { quote: "High heelz, high hopes.", author: "George Washington" },
+  { quote: "I live in a van down by the river.", author: "Mark Twain" },
+  { quote: "Pull my finger", author: "Shakesphere" },
+  { quote: "God is dog spelled backward.", author: "Plato" },
+  { quote: "Robots have feelings too!", author: "Mark Zukerburg" },
+  { quote: "The shozzberries taste like shozzberries.", author: "Elon Musk" },
+  { quote: "You ain't all that n a bag of chips.", author: "Noam Chomsky" },
+  { quote: "E equals MC Hammer", author: "Einstein" },
+  { quote: "Don't stop belevin.", author: "Motzart" },
+  { quote: "Stupid is as stupid does", author: "Charles Darwin" }
+];
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quote: "Puke and rally.",
+      author: "Ralph Emerson"
+    };
+  }
+
+  handleClick() {
+    const randomItem = myArray[Math.floor(Math.random() * myArray.length)];
+    window.onload = this.state;
+
+    this.setState({
+      quote: randomItem.quote,
+      author: randomItem.author
+    });
+  }
+
+  render() {
+    return (
+      <div className="App container-fluid">
+        <wrapper
+          id="quote-box"
+          className="d-flex align-items-center justify-content-center vh-100">
+          <div id="quote-container" className="col-6 box p-5 rounded">
+            <div id="text" className="text-center display-2">
+              {" "}
+              "{this.state.quote}"
+            </div>
+
+            <div id="author" className="text-right">
+              {this.state.author}
+            </div>
+
+            <div className=" buttons d-flex justify-content-between">
+              <a id="tweet-quote" className="btn btn-secondary active" role="button" aria-pressed="true" href="https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text="
+              >
+                #tweet
+              </a>
+
+              <button
+                id="new-quote"
+                className="btn btn-primary active" 
+                onClick={this.handleClick.bind(this)}
+              >
+                Hit Me!
+              </button>
+            </div>
+          </div>
+        </wrapper>
+      </div>
+    );
+  }
 }
 
 export default App;
